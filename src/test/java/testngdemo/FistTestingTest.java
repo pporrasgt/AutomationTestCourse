dfbe1b65a76f3525365b136b6dbc0fd7134fa413
Pedro Porras <pedroporrasmedina@gmail.com>
2022-05-19 19:34:51 -0600

    Creando XML de TestNG y primer test

---
+++ src/test/java/testngdemo/FistTestingTest.java
@@ -0,0 +1,12 @@
+package testngdemo;
+
+import org.testng.annotations.Test;
+
+public class FistTestingTest {
+    @Test
+    public void firstTestWithTestng(){
+        System.out.println("Primer test con TestNG");
+    }
+
+
+}
